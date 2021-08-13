106e7634bd735f5aacaa0d0167b6781c3ece7998
Tyler Lanphear <tylanphear@gmail.com>
2021-08-13 13:34:26 -0700

    build: refactor make.bash and wfinish.sh
    `make.bash`:
        - Pass a platform to build instead of always building OS X and
          Linux.
        - Call the appropriate `*finish.sh` script according to platform
    
    `wfinish.sh`:
        - Perform each compilation separately and then link.
        - Always pass `-DPLATFORM_WINDOWS`

--- scripts/wfinish.sh
+++ scripts/wfinish.sh
@@ -1,16 +1,23 @@
 #!/usr/bin/env bash
 
+if [[ -z "$CC" ]]; then
+    echo 'Need to set $CC to a valid compiler or cross-compiler' >/dev/stderr
+    exit 2
+fi
+
+CCFLAGS="$CCFLAGS -DPLATFORM_WINDOWS"
+
 "$CC" $CCFLAGS -std=gnu99 -c core/sha256.c -O3 -o sha256.o -fPIC
 "$CC" $CCFLAGS -std=gnu99 -c compiler/cvm.c -O3 -o cvm.o -fPIC
 "$CC" $CCFLAGS -std=gnu99 -c runtime/linenoise-win32.c -O3 -o linenoise.o -fPIC
+"$CC" $CCFLAGS -std=gnu99 -c runtime/driver.c -O3 -o driver.o -fPIC
 "$CC" $CCFLAGS -std=gnu99 \
-    runtime/driver.c \
+    driver.o \
     linenoise.o \
     cvm.o \
     sha256.o \
     wstanza.s \
     -o wstanza \
-    -DPLATFORM_WINDOWS \
     -lm \
     -lpthread \
     -fPIC
